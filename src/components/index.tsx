c0d4a60eb489aff64b35ea13092ccb1c8c089f60
vivianafb <vivianafb15@gmail.com>
2024-09-29 20:21:56 -0300

    Fix(dev): Create next project

---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { search } from "./header/search";
+export { header } from "./header/header";
+export { CardInformation } from "./card-information/CardInformation";
+export { Map } from "./map/Map";
